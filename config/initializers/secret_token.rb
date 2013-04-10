03d8a7575b54b4a2a6aa90d812d2bce9d8993a01
Matthew Carr <bikokid@gmail.com>
2013-04-10 15:27:05 -0700

    initial commit with social stream working without sphinx or chat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Network::Application.config.secret_token = 'f54b45a4a963e7962bbda9c0e8541915144fdf0a34e1185bad995d572a93f30fca2369e17decf9c57593194096847ababe06574191a8eeac8b67dc297e181356'
